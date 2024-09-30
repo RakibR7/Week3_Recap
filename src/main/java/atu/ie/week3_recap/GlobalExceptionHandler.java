0c07ad201f8da42908b87832d2b6da1dc55f6f7b
Rakib Rana <G00408644@atu.ie>
2024-09-30 12:55:12 +0100

    testing v1

---
+++ src/main/java/atu/ie/week3_recap/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package atu.ie.week3_recap;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleValidationException(MethodArgumentNotValidException ex) {
+        return "Error occured" + ex.getBindingResult().getFieldError().getDefaultMessage();
+
+    }
+}
